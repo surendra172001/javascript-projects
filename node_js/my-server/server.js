560f0b4e5629ec342c8046d3fb7409b3895b4f8f
surendra172001 <sap172001@gmail.com>
2020-03-20 18:48:40 +0530

    normal update

--- node_js/my-server/server.js
+++ node_js/my-server/server.js
@@ -1,1 +1,26 @@
-console.log("surendra");
+const express = require('express');
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("<h1>This is my home page</h1>");
+});
+
+app.get("/about", function(req, res) {
+    res.send("<h1>Hi, There everyone my name is Surendra Pandey</h1>");
+});
+
+app.get("/index", function(req, res) {
+    res.sendFile("C:/Users/surendra/Desktop/my_files/customer_user_interface_project/au_tuts/node_js/my-server/index.html");
+});
+
+app.get("/calculate", function(req, res) {
+    res.sendFile("C:/Users/surendra/Desktop/my_files/customer_user_interface_project/au_tuts/node_js/my-server/index.html");
+});
+
+
+const port = 8080;
+
+app.listen(port, function() {
+    console.log("server started listening on port: " + port);
+});
